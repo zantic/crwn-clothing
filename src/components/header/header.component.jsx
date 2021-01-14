30d6dd52f83fbacfbcbf3aa623d7cf73d9a0ff9d
zantic <zoran.antic@gmail.com>
2021-01-14 23:53:19 +0100

    Adds redux

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { auth } from '../../firebase/firebase.utils'
+
 import { ReactComponent as Logo } from '../../assets/crown.svg'
 import './header.styles.scss'
 
@@ -21,4 +23,7 @@ const Header = ({ currentUser }) => (
   </div>
 )
 
-export default Header
+const mapStateToProps = state => ({
+  currentUser: state.user.currentUser,
+})
+export default connect(mapStateToProps)(Header)
